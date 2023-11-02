5e1b9a01f5ae8c5c6f5dd7c24c87499415366a79
joemust0 <joaofss.1991@gmail.com>
2023-11-02 01:17:19 -0300

    Trabalhando com interpolação de dados

---
+++ src/app/components/first-component/first-component.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-first-component',
+  templateUrl: './first-component.component.html',
+  styleUrls: ['./first-component.component.css']
+})
+export class FirstComponentComponent implements OnInit {
+  name: string = 'Joe';
+  age: number = 32;
+  job = "Programador";
+  Hobbies = ['Musica', 'Jogar' , 'Viagens'];
+car={
+  name: 'Honda',
+  year: 1998,
+  transmition: 'Automático',
+};
+
+  constructor() { }
+
+  ngOnInit(): void {
+
+  }
+
+}
