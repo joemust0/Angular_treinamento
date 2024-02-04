2011cfabb622660419f3535a1c56e71e3d6e07c1
joemust0 <joaofelipe.1607@gmail.com>
2024-02-04 17:07:11 -0300

    Atualização dos exercicios

---
+++ src/app/components/eventos/eventos.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-eventos',
+  templateUrl: './eventos.component.html',
+  styleUrls: ['./eventos.component.css']
+})
+export class EventosComponent implements OnInit {
+show: boolean = false;
+
+  constructor() { }
+
+  ngOnInit(): void {}
+  
+    showMessege(): void{
+      this.show = !this.show; //toggle
+    }
+}
