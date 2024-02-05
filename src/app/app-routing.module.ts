5969792a11936dd84b9c5aff34b5aa1861993a56
joemust0 <joaofelipe.1607@gmail.com>
2024-02-04 22:39:49 -0300

    Atualização dos exercicios 17

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FirstComponentComponent } from './components/first-component/first-component.component';
+import { ListRenderComponent } from './components/list-render/list-render.component';
+import { DirectivesComponent } from './components/directives/directives.component';
+import { ParentDataComponent } from './components/parent-data/parent-data.component';
+import { IfRenderComponent } from './components/if-render/if-render.component';
+import { EventosComponent } from './components/eventos/eventos.component';
+import { EmitterComponent } from './components/emitter/emitter.component';
+import { PipeComponent } from './components/pipe/pipe.component';
+import { TwoAwayBlindingComponent } from './components/two-away-blinding/two-away-blinding.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: FirstComponentComponent},
+  {path: 'list', component: ListRenderComponent},
+  {path: 'directives', component: DirectivesComponent},
+  {path: 'parent-data', component: ParentDataComponent},
+  {path: 'if-render', component: IfRenderComponent},
+  {path: 'evertos', component: EventosComponent},
+  {path: 'emitter', component: EmitterComponent},
+  {path: 'pipe', component: PipeComponent},
+  {path: 'TAB', component: TwoAwayBlindingComponent},
+];
 
 @NgModule({
+  declarations: [],
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
