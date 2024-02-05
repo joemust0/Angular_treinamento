76983183f8d2dab8e90288417de0dba0fa2f6d72
joemust0 <joaofelipe.1607@gmail.com>
2024-02-04 22:04:19 -0300

    Atualização dos exercicios 16

--- src/app/components/list-render/list-render.component.ts
+++ src/app/components/list-render/list-render.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Animal } from 'src/app/Animal';
+import { ListService } from 'src/app/services/list.service';
+
 @Component({
   selector: 'app-list-render',
   templateUrl: './list-render.component.html',
@@ -32,7 +34,7 @@ cars= [
 
 animalDetails = '';
 
-constructor() { }
+constructor(private listService:ListService) { }
 
   ngOnInit(): void {  }
 
@@ -40,5 +42,10 @@ constructor() { }
 
   showAge(animal: Animal) { 
     this.animalDetails = `O pet ${animal.name} tem ${animal.age} anos!`;
+  }
+
+  removeAnimal(animal:Animal){
+    console.log('Removendo animnal....')
+    this.animals = this.listService.remove(this.animals,animal);
   }
 }
