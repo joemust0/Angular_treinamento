f71ee05af53bcc5b96f621556f031cfb0a602b5e
joemust0 <joaofelipe.1607@gmail.com>
2024-02-04 21:23:12 -0300

    Atualização dos exercicios

---
+++ src/app/components/list-render/list-render.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { Animal } from 'src/app/Animal';
+@Component({
+  selector: 'app-list-render',
+  templateUrl: './list-render.component.html',
+  styleUrls: ['./list-render.component.css']
+})
+export class ListRenderComponent implements OnInit{
+  animals: Animal[] = [
+    {name: "Lilica", type: "Cachorro", age: 4},
+    {name: "Lua", type: "Gato", age: 5},
+    {name: "Floquino", type: "Cachorro", age: 2},
+    {name: "Spok", type: "Cachorro", age: 10},
+    {name: "Thor", type: "Cavalo", age: 7},
+    {name: "Theodoro", type: "Coelho", age: 1},
+  ];
+
+cars= [
+  {fabricante: "Honda", model: "Civic", type: "Coupe"},
+  {fabricante: "Honda", model: "Accord", type: "sedan"},
+  {fabricante: "Honda", model: "CRV", type: "SUV"},
+  {fabricante: "Toyota", model: "Corrola", type: "sedan"},
+  {fabricante: "Toyota", model: "Hilux", type: "Pick-Up"},
+  {fabricante: "VolksWagen", model: "Gol", type: "hatch"},
+  {fabricante: "VolksWagen", model: "Polo", type: "hatch"},
+];
+
+animalDetails = ''
+
+constructor() { }
+
+  ngOnInit(): void {  }
+
+  underline = 'underline';
+
+  showAge(animal: Animal) { }
+    this.animalDetails = `O pet ${animal.name} tem ${animal.age} anos!`;
+}
